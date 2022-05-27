8af3bc633cebfca684e2f22721d672b72e80902e
danjerezTdf <104039810+danjerezTdf@users.noreply.github.com>
2022-05-27 15:26:44 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+/*Par/Impar
+• Realizar un algoritmo que dado un
+número entero ingresado por el
+usuario, visualice en pantalla si es
+par o impar
+• En el caso de ingresar un cero, se
+debe volver a pedir el número por
+teclado (hasta que se ingrese un
+número mayor que cero)*/
+
 let numeroValor: number = Number(prompt(`Ingreser Valor a comprobar`));
 //console.log(numeroValor);
 while (numeroValor === 0) {
